303bab1725e6c87032ec603b2a8c8f5e9493a5b3
Laurent Grangeau <laurent.grangeau@gmail.com>
2014-08-25 22:03:36 +0200

    Add base classes

---
+++ Dali/Dali/MapperBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Dali
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Data;
+
+    public abstract class MapperBase<T>
+    {
+        protected abstract T Map(IDataReader dataReader);
+
+        protected abstract IList<T> MapAll(IDataReader dataReader)
+        {
+            var result = new List<T>();
+
+            while (dataReader.Read())
+            {
+                result.Add(Map(dataReader));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Dali/Dali/ReaderBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Dali
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class ReaderBase<T>
+    {
+        public IList<T> Read()
+        {
+            var result = new List<T>();
+
+            return result;
+        }
+    }
+}
